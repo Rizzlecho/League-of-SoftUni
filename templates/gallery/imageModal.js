2bc169824a347fe48fdb1b324d29d58e800d8ef7
Cvetomir Shkondachki <Cvetomir Shkondachki>
2016-12-20 15:48:43 +0200

    galleymodal

---
+++ templates/gallery/imageModal.js
@@ -0,0 +1,20 @@
+// Get the modal
+var modal1 = document.getElementById('myModal1');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg1');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption1");
+img.onclick = function(){
+    modal1.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal1.style.display = "none";
+}
